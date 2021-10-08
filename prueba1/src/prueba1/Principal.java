e3255bc0528d92b17ab8c655ec610fee316f140d
profesor <profesor@DESKTOP-1MMKTVN>
2021-10-08 13:58:35 +0200

    Validación del tercer cambio

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git version 2");
+		System.out.println("Esta es la versión 3");
 	}
 
 }
