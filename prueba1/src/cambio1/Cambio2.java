173bc5999e3e3eb57133515953de81f351d343ca
profesor <profesor@DESKTOP-1MMKTVN>
2021-10-18 09:00:01 +0200

    Valido cambio 2 en proyecto 2

---
+++ prueba1/src/cambio1/Cambio2.java
@@ -0,0 +1,10 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase cambio2 en proyecto 2");
+		System.out.println("Clase cambio2 en proyecto 2");
+	}
+
+}
